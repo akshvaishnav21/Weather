d10dc90c28bd58c066ebef1543744213189dc9bd
akshvaishnav2194 <akshvaishnav2194@gmail.com>
2016-01-05 20:44:44 +0530

    Adding okhttp

--- app/src/main/java/com/aksh/weather/MainActivity.java
+++ app/src/main/java/com/aksh/weather/MainActivity.java
@@ -3,11 +3,20 @@ package com.aksh.weather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import okhttp3.OkHttpClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String apiKey = "961ce4e9dff8e58324b2574120dd6c62";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl = "https://api.forecast.io/forecast/"
+                +apiKey +"/"+latitude +","+longitude;
+
+        OkHttpClient client = new OkHttpClient();
     }
 }
