17ab212cff96198c85b5afb10476b360fc65d2de
akshvaishnav2194 <akshvaishnav2194@gmail.com>
2016-01-06 01:10:01 +0530

    Completed the design

--- app/src/main/java/com/aksh/weather/MainActivity.java
+++ app/src/main/java/com/aksh/weather/MainActivity.java
@@ -28,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String apiKey = "961ce4e9dff8e58324b2574120dd6c62";
-        double latitude = 37.8267;
-        double longitude = -122.423;
+        double latitude = 26.2567;
+        double longitude = 73.0170;
         String forecastUrl = "https://api.forecast.io/forecast/"
                 +apiKey +"/"+latitude +","+longitude;
         if (isNetworkAvailable()) {
