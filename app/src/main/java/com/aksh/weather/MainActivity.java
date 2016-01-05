2ba685009254f3f723c6387cf1570b532840476b
akshvaishnav2194 <akshvaishnav2194@gmail.com>
2016-01-05 21:49:34 +0530

    Setting up an error message

--- app/src/main/java/com/aksh/weather/MainActivity.java
+++ app/src/main/java/com/aksh/weather/MainActivity.java
@@ -39,10 +39,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Response response) throws IOException {
                 try {
+                    Log.v(TAG, response.body().string());
 
                     if (response.isSuccessful()){
-                        Log.v(TAG, response.body().string());
-
+                        
+                    }
+                    else {
+                    alertUserAboutError();
                     }
 
                 } catch (IOException e) {
@@ -52,6 +55,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        Log.d(TAG, "Main UI code is running.");
 
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
