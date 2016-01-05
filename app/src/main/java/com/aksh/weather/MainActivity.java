f4378f864d42201239c013192d8c6f6717d9a12a
akshvaishnav2194 <akshvaishnav2194@gmail.com>
2016-01-05 21:11:23 +0530

    Checking for Response

--- app/src/main/java/com/aksh/weather/MainActivity.java
+++ app/src/main/java/com/aksh/weather/MainActivity.java
@@ -2,11 +2,19 @@ package com.aksh.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +26,32 @@ public class MainActivity extends AppCompatActivity {
                 +apiKey +"/"+latitude +","+longitude;
 
         OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+
+                    if (response.isSuccessful()){
+                        Log.v(TAG, response.body().string());
+
+                    }
+
+                } catch (IOException e) {
+                    Log.e(TAG,"Exception Caught :",e);
+                }
+
+
+            }
+        });
+
     }
 }
